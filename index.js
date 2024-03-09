4f207bac13df0ee47cc2468c390a35e362f0809c
Dhruv-3241 <dhruv.25977@gmail.com>
2024-03-09 19:12:06 +0530

    added 0.0.0.0 for render to listen

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use("/api", router);
 
-app.listen(4000, () => {
+app.listen(4000,'0.0.0.0', () => {
   console.log('Server is running on port 4000');
 });
 
